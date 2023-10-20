4b802e83f3b7cb5a4d7692819fcbee6bd7e1ec2f
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-10-20 13:40:24 -0400

    Update lichen_assembly_SAMN08225147.sh

--- Assemblies/lichen_assembly_SAMN08225147.sh
+++ Assemblies/lichen_assembly_SAMN08225147.sh
@@ -8,8 +8,6 @@
 #SBATCH --mem-per-cpu=20gb # request memory, adjustable
 #SBATCH --error=special_cases.%J.err
 #SBATCH --output=special_cases.%J.out
-#SBATCH --mail-user=samche42@gmail.com
-#SBATCH --mail-type=END,FAIL
 
 cd Special_cases
 
