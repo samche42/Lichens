4e84df8faf5963691f911f6384dcfb8e0f4b73d4
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-11-22 09:00:33 -0500

    Update CAT.sh

--- Annotation_scripts/CAT.sh
+++ Annotation_scripts/CAT.sh
@@ -9,6 +9,9 @@
 #SBATCH --error=CAT.%J.err
 #SBATCH --output=CAT.%J.out
 
+#Looped through all fasta files with the following command:
+#for file in Lichen_assemblies/*.fasta; do sbatch CAT.sh -i ${file};done
+
 usage()
 {
    # Display Help
@@ -32,18 +35,18 @@ source activate CAT
 CAT contigs -c ${input_file} \
     -d CAT_prepare_20210107/2021-01-07_CAT_database \
     -t CAT_prepare_20210107/2021-01-07_taxonomy \
-    --out_prefix ${input_file/_scaffolds_3000bp_removed.fasta/''} \
+    --out_prefix ${input_file/_scaffolds.fasta/''} \
     --nproc 16 \
     --I_know_what_Im_doing \
     --top 11
 
-CAT add_names -i ${input_file/_scaffolds_3000bp_removed.fasta/''}.contig2classification.txt \
-    -o ${input_file/_scaffolds_3000bp_removed.fasta/''}.official_names.txt \
+CAT add_names -i ${input_file/_scaffolds.fasta/''}.contig2classification.txt \
+    -o ${input_file/_scaffolds.fasta/''}.official_names.txt \
     -t CAT_prepare_20210107/2021-01-07_taxonomy \
     --only_official
 
 CAT summarise -c ${input_file} \
-    -i ${input_file/_scaffolds_3000bp_removed.fasta/''}.official_names.txt \
-    -o ${input_file/_scaffolds_3000bp_removed.fasta/''}.summary.txt
+    -i ${input_file/_scaffolds.fasta/''}.official_names.txt \
+    -o ${input_file/_scaffolds.fasta/''}.summary.txt
 
 conda deactivate
