33884e8cf0ca70fda04c8f775edd213362229efe
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-12-12 15:04:11 -0500

    Update CAT.sh

--- Annotation_scripts/CAT.sh
+++ Annotation_scripts/CAT.sh
@@ -6,8 +6,8 @@
 #SBATCH -n 1 # Tasks # try to increase task number and see any change
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=40gb # request memory, adjustable
-#SBATCH --error=/home/simonsonsc/Slurm_scripts/log_files/CAT.%J.err
-#SBATCH --output=/home/simonsonsc/Slurm_scripts/log_files/CAT.%J.out
+#SBATCH --error=CAT.%J.err
+#SBATCH --output=CAT.%J.out
 #SBATCH --mail-user=samche42@gmail.com
 #SBATCH --mail-type=FAIL
 
@@ -29,13 +29,13 @@ do
     esac
 done
 
-cd /mnt/projects/abcs-dssb/mtp/slurm/simonsonsc/Lichen_project/Acceptable_assemblies/
+cd Acceptable_assemblies/
 
-source /mnt/nasapps/development/python/miniconda3/3.10/bin/activate CAT
+source activate CAT
 
 CAT contigs -c ${input_file} \
-    -d /mnt/projects/bioinfo/legacy/SeqIdx/cat/CAT_prepare_20210107/2021-01-07_CAT_database \
-    -t /mnt/projects/bioinfo/legacy/SeqIdx/cat/CAT_prepare_20210107/2021-01-07_taxonomy \
+    -d /cat/CAT_prepare_20210107/2021-01-07_CAT_database \
+    -t cat/CAT_prepare_20210107/2021-01-07_taxonomy \
     --out_prefix ${input_file/_scaffolds.fasta/''} \
     --nproc 16 \
     --I_know_what_Im_doing \
@@ -43,7 +43,7 @@ CAT contigs -c ${input_file} \
 
 CAT add_names -i ${input_file/_scaffolds.fasta/''}.contig2classification.txt \
     -o ${input_file/_scaffolds.fasta/''}.official_names.txt \
-    -t /mnt/projects/bioinfo/legacy/SeqIdx/cat/CAT_prepare_20210107/2021-01-07_taxonomy \
+    -t /cat/CAT_prepare_20210107/2021-01-07_taxonomy \
     --only_official
 
 conda deactivate
