9bf51f5b709b4567d45d38d8252fd54cc96da873
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-12-29 13:53:38 -0500

    Update contig_counter.py

--- Misc_scripts/contig_counter.py
+++ Misc_scripts/contig_counter.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import os
 import pandas as pd
 import numpy as np
@@ -14,14 +13,14 @@ accessions = [(((os.path.splitext(file))[0]).split("_"))[0] for file in os.listd
 
 def get_final_df(accession):
     #Read in CAT file
-    CAT_df = pd.read_csv("SRR13685122"+'.official_names.txt', sep = "\t", usecols=['# contig','superkingdom'])
+    CAT_df = pd.read_csv(args.accession+'.official_names.txt', sep = "\t", usecols=['# contig','superkingdom'])
     CAT_df['superkingdom'] = CAT_df['superkingdom'].str.replace(':.*', '', regex=True)
     CAT_df.columns = ['contig','CAT_superkingdom']
     CAT_df['CAT_superkingdom'] = CAT_df['CAT_superkingdom'].replace(np.nan,'Unclassified')
     CAT_df['CAT_superkingdom'] = CAT_df['CAT_superkingdom'].str.replace('no support','Unclassified')
 
     #Read in MMSeqs2 file
-    mmseqs_df = pd.read_csv("SRR13685122"+'_mmseqs_taxonomy_final.txt', sep = "\t", header=None,usecols=[0,8])
+    mmseqs_df = pd.read_csv(args.accession+'_mmseqs_taxonomy_final.txt', sep = "\t", header=None,usecols=[0,8])
     mmseqs_df.columns = ['contig','MMSeqs_taxonomy']
     mmseqs_df['MMSeqs_superkingdom'] = mmseqs_df['MMSeqs_taxonomy'].str.split(';').str[1]
     mmseqs_df['MMSeqs_superkingdom'] = mmseqs_df['MMSeqs_superkingdom'].str.replace('d_', '')
@@ -32,7 +31,6 @@ def get_final_df(accession):
 
     #Merge dataframes
     final_df = pd.merge(mmseqs_df, CAT_df, on = 'contig',how = 'outer')
-    #final_df['MMSeqs_superkingdom'] = final_df['MMSeqs_superkingdom'].replace(np.nan,'Unclassified')
     final_df = final_df.replace('Unclassified', np.nan)
 
     #Get majority vote
