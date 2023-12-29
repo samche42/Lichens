dcb7c26ba80723adb32fa501d4b663f2abc841f3
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-12-29 13:10:54 -0500

    Update and rename kingdom_binner.py to superkingdom_binner.py

--- Misc_scripts/superkingdom_binner.py
+++ Misc_scripts/superkingdom_binner.py
@@ -8,20 +8,15 @@ parser.add_argument("-a", "--accession", help="Full file path to kegg input file
 
 args = parser.parse_args()
 
-#Read in Autometa file
-auto_df = pd.read_csv(args.accession+'.taxonomy.tsv', sep = "\t", usecols=['contig','superkingdom'])
-auto_df.superkingdom = auto_df.superkingdom.str.capitalize()
-auto_df.columns = ['contig','Autometa_superkingdom']
-
 #Read in CAT file
-CAT_df = pd.read_csv(args.accession+'.official_names.txt', sep = "\t", usecols=['# contig','superkingdom'])
+CAT_df = pd.read_csv("SRR13685122"+'.official_names.txt', sep = "\t", usecols=['# contig','superkingdom'])
 CAT_df['superkingdom'] = CAT_df['superkingdom'].str.replace(':.*', '', regex=True)
 CAT_df.columns = ['contig','CAT_superkingdom']
 CAT_df['CAT_superkingdom'] = CAT_df['CAT_superkingdom'].replace(np.nan,'Unclassified')
 CAT_df['CAT_superkingdom'] = CAT_df['CAT_superkingdom'].str.replace('no support','Unclassified')
 
 #Read in MMSeqs2 file
-mmseqs_df = pd.read_csv(args.accession+'_mmseqs_taxonomy_final.txt', sep = "\t", header=None,usecols=[0,8])
+mmseqs_df = pd.read_csv("SRR13685122"+'_mmseqs_taxonomy_final.txt', sep = "\t", header=None,usecols=[0,8])
 mmseqs_df.columns = ['contig','MMSeqs_taxonomy']
 mmseqs_df['MMSeqs_superkingdom'] = mmseqs_df['MMSeqs_taxonomy'].str.split(';').str[1]
 mmseqs_df['MMSeqs_superkingdom'] = mmseqs_df['MMSeqs_superkingdom'].str.replace('d_', '')
@@ -31,17 +26,21 @@ mask = mmseqs_df['MMSeqs_superkingdom'].str.contains("vir")
 mmseqs_df.loc[mask, 'MMSeqs_superkingdom'] = "Viruses"
 
 #Merge dataframes
-CAT_auto_df = pd.merge(auto_df, CAT_df, on = 'contig',how = 'outer')
-CAT_auto_df['Autometa_superkingdom'] = CAT_auto_df['Autometa_superkingdom'].replace(np.nan,'Unclassified')
-final_df = pd.merge(CAT_auto_df, mmseqs_df, on = 'contig',how = 'outer')
-final_df['MMSeqs_superkingdom'] = final_df['MMSeqs_superkingdom'].replace(np.nan,'Unclassified')
+final_df = pd.merge(mmseqs_df, CAT_df, on = 'contig',how = 'outer')
+#final_df['MMSeqs_superkingdom'] = final_df['MMSeqs_superkingdom'].replace(np.nan,'Unclassified')
+final_df = final_df.replace('Unclassified', np.nan)
 
 #Get majority vote
 mostCommonVote=[]
-for row in final_df[['Autometa_superkingdom', 'CAT_superkingdom','MMSeqs_superkingdom']].values:
+for row in final_df[['CAT_superkingdom','MMSeqs_superkingdom']].values:
+    row = [i for i in row if i is not np.nan]
     votes, values = np.unique(row, return_counts=True)
-    if np.all(values<=1):
-            mostCommonVote.append("Unclassified")
+    if len(values) == 0: #If they were both unclassified, assign "Unclassified" as the final decision
+        mostCommonVote.append("Unclassified")
+    elif len(values) == 1: #If only one tool managed to classify the contig, go with that assignment
+        mostCommonVote.append(votes[0])
+    elif np.all(values<=1): #If there's a split vote, assign "Unclassified"
+        mostCommonVote.append("Unclassified")
     else:
         mostCommonVote.append(votes[np.argmax(values)])
 
@@ -51,14 +50,10 @@ final_df['Final_classification'] = mostCommonVote
 kingdoms = ['Eukaryota', 'Unclassified', 'Bacteria', 'Archaea', 'Viruses']
 
 def extract_kingdom_contigs(fasta_file, kingdom):
-    contig_ids = final_df.loc[final_df['Final_classification'] == kingdom, 'contig'] # Get contig IDs for the specified kingdom
-    contig_ids_set = set(contig_ids) # Create a set for faster membership checking
-    output_file = f"{fasta_file.split('_')[0]}_{kingdom}.fasta"
-    # Filter and write contigs directly based on membership in the set
-    with open(output_file, "w") as output_handle:
-        for record in SeqIO.parse(fasta_file, "fasta"):
-            if record.id in contig_ids_set:
-                SeqIO.write(record, output_handle, "fasta")
+    contigs = final_df[final_df['Final_classification'] == kingdom]['contig'].tolist()
+    output_file = fasta_file.split("_")[0] + "_"+kingdom+".fasta"
+    records = (r for r in SeqIO.parse(fasta_file, "fasta") if r.id in contigs)
+    SeqIO.write(records, output_file, "fasta")
 
 for kingdom in kingdoms:
     extract_kingdom_contigs(args.accession+'_scaffolds.fasta', kingdom)
