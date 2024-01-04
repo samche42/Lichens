0010b071ce3efdd173a4291d245e2b5940bee4f9
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-01-04 13:49:10 -0500

    Update eukary_phylum_summary.py

--- Misc_scripts/eukary_phylum_summary.py
+++ Misc_scripts/eukary_phylum_summary.py
@@ -32,6 +32,8 @@ def get_phyla(accession,fasta_file,tax_file,scaffold_file):
     scaffold_df = scaffold_df.astype({'cov':'float'})
     total_coverage = scaffold_df['cov'].sum()
     df['rel_cov'] = df['cov']/total_coverage*100
+    total_coverage_over_3000 = df['cov'].sum()
+    df['rel_cov_over_3000'] = df['cov']/total_coverage_over_3000*100
     
     mmseqs_df = pd.read_csv(tax_file, sep = "\t", header=None,usecols=[0,8])
     mmseqs_df.columns = ['contig','MMSeqs_taxonomy']
@@ -51,11 +53,15 @@ def get_phyla(accession,fasta_file,tax_file,scaffold_file):
     length_df = final_df.groupby('MMSeqs_phylum')['length'].sum()
     length_df = length_df.reset_index()
     length_df = length_df.rename(columns = {'length':accession})
-    return counts_df, abundance_df, length_df
+    abundance_df2 = final_df.groupby('MMSeqs_phylum')['rel_cov_over_3000'].sum()
+    abundance_df2 = abundance_df.reset_index()
+    abundance_df2 = abundance_df.rename(columns = {'rel_cov_over_3000':accession})
+    return counts_df, abundance_df, length_df, abundance_df2
 
 final_count_df = pd.DataFrame(columns =['MMSeqs_phylum'])
 final_abund_df = pd.DataFrame(columns =['MMSeqs_phylum'])
 final_length_df = pd.DataFrame(columns =['MMSeqs_phylum'])
+final_abund2_df = pd.DataFrame(columns =['MMSeqs_phylum'])
 
 for acc in accessions:
     scaffold_file = acc+"_scaffolds.fasta"
@@ -71,16 +77,22 @@ for acc in accessions:
         length = final_length_df.loc[:, ['MMSeqs_phylum']]
         rows_to_add_length = final_length_df.shape[0]
         length[acc] = [0]*rows_to_add_length
+        abund2 = final_abund2_df.loc[:, ['MMSeqs_phylum']]
+        rows_to_add_abund2 = final_abund2_df.shape[0]
+        abund2[acc] = [0]*rows_to_add_abund2
     else:
-        count,abund,length = get_phyla(acc,fasta_file,tax_file,scaffold_file)
+        count,abund,length,abund2 = get_phyla(acc,fasta_file,tax_file,scaffold_file)
     final_count_df = pd.merge(final_count_df, count, on = 'MMSeqs_phylum', how = 'outer')
     final_abund_df = pd.merge(final_abund_df, abund, on = 'MMSeqs_phylum', how = 'outer')
     final_length_df = pd.merge(final_length_df, length, on = 'MMSeqs_phylum', how = 'outer')
+    final_abund2_df = pd.merge(final_abund2_df, abund2, on = 'MMSeqs_phylum', how = 'outer')
 
 final_count_df = final_count_df.fillna('0')
 final_abund_df = final_abund_df.fillna('0')
 final_length_df = final_length_df.fillna('0')
+final_abund2_df = final_abund2_df.fillna('0')
 
 final_count_df.to_csv("Eukary_phyla_counts.txt", sep='\t', index=False)
 final_abund_df.to_csv("Eukary_phyla_abundances.txt", sep='\t', index=False)
 final_length_df.to_csv("Eukary_phyla_lengths.txt", sep='\t', index=False)
+final_abund2_df.to_csv("Eukary_phyla_abundances_over_3000bp.txt", sep='\t', index=False)
