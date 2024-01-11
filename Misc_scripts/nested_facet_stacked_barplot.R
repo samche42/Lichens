178deb29dd47f7cf34f153eb957218dcc4875a0d
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-01-11 12:23:44 -0500

    Update nested_facet_stacked_barplot.R

--- Misc_scripts/nested_facet_stacked_barplot.R
+++ Misc_scripts/nested_facet_stacked_barplot.R
@@ -170,3 +170,71 @@ ggplot() +
     axis.ticks.x = element_blank()
   ) +
   guides(fill = guide_legend(title = ''))
+
+################################################################################################
+#
+# FULL BGC length per sample, coloured by type,  faceted by hierarchacal taxonomy
+#
+################################################################################################
+
+###################
+##Contig edge BGCs
+###################
+
+library(ggplot2)
+library(reshape2)
+library(ggh4x)
+
+data = read.csv("Contig_edge_BGC_counts.txt", header=TRUE, sep ="\t")
+melted = melt(data, id = c("Sample_name","Superkingdom","Phylum","Class","Order","Family","Genus","Species"))
+
+melted$Class <- factor(melted$Class, levels = unique(melted$Class))
+melted$Order <- factor(melted$Order, levels = unique(melted$Order))
+melted$Family <- factor(melted$Family, levels = unique(melted$Family))
+melted$Genus <- factor(melted$Genus, levels = unique(melted$Genus))
+melted$Sample_name <- factor(melted$Sample_name, levels = unique(melted$Sample_name))
+
+#Getting distinct colours
+color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+custom_colors=sample(color, 11)
+
+ggplot() +
+  geom_bar(data = melted,aes(x = Sample_name, y =  value, fill = variable), stat = "identity", width = 1,position = position_stack(reverse = TRUE)) +
+  scale_fill_manual(values = custom_colors) +
+  theme_bw() +
+  facet_nested(. ~ Class + Order +  Family, scales = "free", space = "free") +
+  theme(strip.text.x = element_text(angle = 90,size=5),
+        panel.spacing=unit(0,"lines"),
+        panel.background = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+###################
+##Complete BGCs
+###################
+
+data = read.csv("Complete_BGC_counts.txt", header=TRUE, sep ="\t")
+melted = melt(data, id = c("Sample_name","Superkingdom","Phylum","Class","Order","Family","Genus","Species"))
+
+melted$Class <- factor(melted$Class, levels = unique(melted$Class))
+melted$Order <- factor(melted$Order, levels = unique(melted$Order))
+melted$Family <- factor(melted$Family, levels = unique(melted$Family))
+melted$Genus <- factor(melted$Genus, levels = unique(melted$Genus))
+melted$Sample_name <- factor(melted$Sample_name, levels = unique(melted$Sample_name))
+
+#Getting distinct colours
+color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+custom_colors=sample(color, 11)
+
+ggplot() +
+  geom_bar(data = melted,aes(x = Sample_name, y =  value, fill = variable), stat = "identity", width = 1,position = position_stack(reverse = TRUE)) +
+  scale_fill_manual(values = custom_colors) +
+  theme_bw() +
+  facet_nested(. ~ Class + Order +  Family, scales = "free", space = "free") +
+  theme(strip.text.x = element_text(angle = 90,size=5),
+        panel.spacing=unit(0,"lines"),
+        panel.background = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
