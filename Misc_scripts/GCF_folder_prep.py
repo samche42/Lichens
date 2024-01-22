2908b1fcdd1031892457c7da935e846c47ab2846
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-01-22 12:01:33 -0500

    Create GCF_folder_prep.py

---
+++ Misc_scripts/GCF_folder_prep.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+import shutil
+
+df = pd.read_csv("GCF_assignment.txt", sep="\t", header=0)
+
+#Get list pf GCFs fol folder names
+unique_values = df['GCF'].unique()
+
+# Create folders for each GCF
+for value in unique_values:
+    os.makedirs(f"GCF_{value}", exist_ok=True)
+
+# Move files to their assigned folders
+for index, row in df.iterrows():
+    filename = row['File']
+    gcf_value = row['GCF']
+    GCF_folder = f"GCF_{gcf_value}"
+    shutil.move(filename, GCF_folder)
